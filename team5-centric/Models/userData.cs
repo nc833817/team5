2c57fbbc713e040339775a68598671391144efc9
Connor Parrish <cp950716@ohio.edu>
2020-10-14 11:34:49 -0400

    Added validation to the userData Model

--- team5-centric/Models/userData.cs
+++ team5-centric/Models/userData.cs
@@ -12,9 +12,23 @@ namespace team5_centric.Models
     {
         [Key]
         public Guid userId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is a required field")]
+        [StringLength(20)]
         public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is a required field")]
+        [StringLength(20)]
         public string lastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address is a required field")]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email Address")]
         public string email { get; set; }
+
+        [Display(Name = "Full Name")]
         public string fullName 
         {
             get
@@ -22,8 +36,21 @@ namespace team5_centric.Models
                 return lastName + ", " + firstName;
             }
         }
+
+        [Display(Name = "Office Location")]
+        [Required(ErrorMessage = "Office Location is a required field")]
+        [StringLength(20)]
         public string officeLocation { get; set; }
+
+        [Display(Name = "Position at Centic")]
+        [Required(ErrorMessage = "Position at Centric is a required field")]
+        [StringLength(20)]
         public string position { get; set; }
+
+        [Display(Name = "Starting Date")]
+        [Required(ErrorMessage = "Starting Date is a required field")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime startDate { get; set; }
         public byte[] avatar { get; set; }
         public ICollection<recognition> recognitions { get; set; }
